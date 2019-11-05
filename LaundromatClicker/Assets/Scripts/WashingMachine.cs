ad918129cf62c9e8e1f533c77f1242498a7454af
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2019-11-05 14:13:59 +0000

    Finished creating clothes, fireworks and confetti particle systems and implemented washing machine mouse click and animation manager, created canvas and help button (yet to be implemented)

---
+++ LaundromatClicker/Assets/Scripts/WashingMachine.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class WashingMachine : MonoBehaviour
+{
+    public AnimationClip animationClip;
+
+    private Animator animator;
+    private ParticleSystem clothesParticleEffect;
+
+    private bool beingClickedOn = false;
+    private float clickIntervalToStopAnimating;
+    private float currentClickInterval = 0;
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+        clothesParticleEffect = GetComponentInChildren<ParticleSystem>();
+        clickIntervalToStopAnimating = animationClip.length;
+    }
+
+    public void Update()
+    {
+        if(beingClickedOn)
+        {
+            currentClickInterval += Time.deltaTime;
+            animator.SetBool("BeingClickedOn", true);
+        }
+
+        if(currentClickInterval > clickIntervalToStopAnimating)
+        {
+            beingClickedOn = false;
+            animator.SetBool("BeingClickedOn", false);
+            currentClickInterval = 0;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        beingClickedOn = true;
+        clothesParticleEffect.Play();
+    }
+}
